ba10859b1da173d7689d5c43e5d17b061f96aeac
hyehh <81559324+hyehh@users.noreply.github.com>
2021-08-21 18:43:15 +0900

    2021.08.21 18:43 조혜지

--- TodoList/TodoList/Calendar/CalendarViewController.swift
+++ TodoList/TodoList/Calendar/CalendarViewController.swift
@@ -16,7 +16,7 @@ class CalendarViewController: UIViewController {
     
     var db: OpaquePointer?
     let formatter = DateFormatter()
-    var dates = [Date]()
+    var dotList = [Date]()
     var calendarList: [Todolist] = []
     let now = Date()
     var tDate: String = ""
@@ -33,9 +33,11 @@ class CalendarViewController: UIViewController {
         
         calendarTableView.delegate = self
         calendarTableView.dataSource = self
+        print("여기?")
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        dotSetting()
         readValues()
     }
     
@@ -54,35 +56,52 @@ class CalendarViewController: UIViewController {
     
     func initDesignSetting() {
         calendar.locale = Locale(identifier: "ko_KR")
-        calendar.today = nil
 
         calendar.appearance.headerMinimumDissolvedAlpha = 0.0
         calendar.appearance.headerDateFormat = "YYYY년 MM월"
         calendar.appearance.weekdayTextColor = .black
         calendar.appearance.headerTitleColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         calendar.appearance.headerTitleFont = .boldSystemFont(ofSize: 20)
+        calendar.appearance.todayColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         calendar.appearance.selectionColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         calendar.appearance.eventSelectionColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         calendar.appearance.eventDefaultColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         
         calendarTableView.separatorStyle = .none
-        // calendar.selectedDate
     }
     
     func initFuncSetting() {
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "yyyy-MM-dd"
         tDate = formatter.string(from: now)
-              
-        let xmas = formatter.date(from: "2021-08-25")
-        let sampledate = formatter.date(from: "2021-08-22")
-        dates = [xmas!, sampledate!]
+    }
+    
+    func dotSetting() {
+        dotList.removeAll()
+        
+        let queryString = "SELECT tDate FROM todo GROUP BY tDate"
+        print(queryString)
+        
+        var stmt: OpaquePointer?
+
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing select: \(errmsg)")
+            return
+        }
+        
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            let tDate = formatter.date(from: String(cString: sqlite3_column_text(stmt, 0)))
+            
+            dotList.append(tDate!)
+        }
+        self.calendarTableView.reloadData()
     }
     
     func readValues(){
         calendarList.removeAll()
         
-        let queryString = "SELECT id, tDate, tList FROM todo where tDate = '\(tDate)'"
+        let queryString = "SELECT id, tDate, tList, tState, tStar FROM todo where tDate = '\(tDate)' ORDER BY tStar DESC"
         print(queryString)
         
         var stmt: OpaquePointer?
@@ -97,13 +116,25 @@ class CalendarViewController: UIViewController {
             let id = sqlite3_column_int(stmt, 0)
             let tDate = String(cString: sqlite3_column_text(stmt, 1))
             let tList = String(cString: sqlite3_column_text(stmt, 2))
+            let tState = String(cString: sqlite3_column_text(stmt, 3))
+            let tStar = String(cString: sqlite3_column_text(stmt, 4))
+            // print(tList)
             
-            calendarList.append(Todolist(id: Int(id), tDate: tDate, tList: tList))
+            calendarList.append(Todolist(id: Int(id), tDate: tDate, tList: tList, tState: tState, tStar: tStar))
+            // print(calendarList[1].tList as Any)
+            // print(calendarList.count)
         }
         self.calendarTableView.reloadData()
         
     }
 
+    @IBAction func btnUnCheck(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnCheck(_ sender: UIButton) {
+        
+    }
     /*
     // MARK: - Navigation
 
@@ -120,7 +151,10 @@ extension CalendarViewController : FSCalendarDelegate, FSCalendarDataSource, FSC
     
     // 날짜 선택 시 콜백 메소드
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        calendar.today = nil
         print(formatter.string(from: date) + " 선택됨")
+        tDate = formatter.string(from: date)
+        readValues()
     }
     // 날짜 선택 해제 시 콜백 메소드
     public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
@@ -129,7 +163,7 @@ extension CalendarViewController : FSCalendarDelegate, FSCalendarDataSource, FSC
     
     // 날짜 밑에 점 찍기
     func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-        if self.dates.contains(date){
+        if self.dotList.contains(date){
             return 1
         }
         return 0
@@ -140,39 +174,52 @@ extension CalendarViewController : FSCalendarDelegate, FSCalendarDataSource, FSC
 extension CalendarViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = calendarTableView.dequeueReusableCell(withIdentifier: "calendarCell") as! CalendarTableViewCell
+        let selectToDoContent = calendarList[indexPath.row]
+        
+        cell.lblTodo.text = selectToDoContent.tList
         
         cell.btnUncheck.tintColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         cell.btnCheck.tintColor = UIColor(displayP3Red: 65/255, green: 99/225, blue: 135/255, alpha: 1)
         
-        cell.btnUncheck.isHidden = true
-        cell.btnCheck.isHidden = false
-        cell.lblTodo.text = "안녕하세요"
-        // cell.lblTodo.attributedText = attributeString
-        // attributeString.addAttribute(.baselineOffset, value: 0, range: (text as NSString).range(of:"Zedd"))
+        if selectToDoContent.tState == "1" {
+            cell.btnUncheck.isHidden = true
+            cell.btnCheck.isHidden = false
+        }else {
+            cell.btnUncheck.isHidden = false
+            cell.btnCheck.isHidden = true
+        }
         
-        let attributedText = NSMutableAttributedString(string: cell.lblTodo.text!)
-        cell.lblTodo.attributedText = attributedText
+        cell.btnUncheck.tag = indexPath.row
+        cell.btnCheck.tag = indexPath.row
+        
+//        let todo = NSMutableAttributedString(string: cell.lblTodo.text!)
+//
+//        cell.lblTodo.attributedText = todo
+//        todo.addAttribute(.baselineOffset, value: 0, range: (cell.lblTodo.text! as NSString).range(of:cell.lblTodo.text!))
+//
+//        let attributedText = NSMutableAttributedString(string: cell.lblTodo.text!)
+//        cell.lblTodo.attributedText = attributedText
 
         // 취소선 구현
 //        let todo = NSMutableAttributedString(string: cell.lblTodo.text!)
 //        todo.beauty.align(.center).strikethrough(1)
 //        cell.lblTodo.attributedText = todo
         
-        attributedText.addAttribute(.baselineOffset, value: 0, range: (cell.lblTodo.text! as NSString).range(of:cell.lblTodo.text!))
-        attributedText.addAttribute(.strikethroughStyle, value: 1, range: (cell.lblTodo.text! as NSString).range(of:cell.lblTodo.text!))
+//        attributedText.addAttribute(.baselineOffset, value: 0, range: (cell.lblTodo.text! as NSString).range(of:cell.lblTodo.text!))
+//        attributedText.addAttribute(.strikethroughStyle, value: 1, range: (cell.lblTodo.text! as NSString).range(of:cell.lblTodo.text!))
 
         
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return calendarList.count
     }
 
 }
 
 extension CalendarViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        return 50
     }
 }
--- TodoList/TodoList/DTO/TodoList.swift
+++ TodoList/TodoList/DTO/TodoList.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 class Todolist{
-    var id :Int
+    var id : Int?
     var tDate : String?
     var tList : String?
     var tState : String?
     var tStar : String?
     
-    init(id: Int, tDate:String?, tList:String?, tState:String?, tStar:String?) {
+    init(id: Int?, tDate:String?, tList:String?, tState:String?, tStar:String?) {
         self.id = id
         self.tDate = tDate
         self.tList = tList
@@ -22,10 +22,15 @@ class Todolist{
         self.tStar = tStar
     }
     
-    init(id: Int, tDate:String?, tList:String?) {
+    init(id: Int?, tDate:String?, tList:String?) {
         self.id = id
         self.tDate = tDate
         self.tList = tList
+    }
+    
+    // 2021.08.21 조혜지 CalendarViewController에서 필요한 Dto 추가
+    init(tDate: String?) {
+        self.tDate = tDate
     }
     
 }
